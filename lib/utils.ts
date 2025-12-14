5e2fcbf8ca9407316ad3f2998ec570ecb7289023
luizdevjava <primeiragp@proton.me>
2025-12-13 22:49:19 -0300

    Add files via upload
    v2

---
+++ lib/utils.ts
@@ -0,0 +1,26 @@
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(amount)
+}
+
+export function formatDate(date: Date | string): string {
+  return new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(new Date(date))
+}
+
+export function formatDateTime(date: Date | string): string {
+  return new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(new Date(date))
+}
